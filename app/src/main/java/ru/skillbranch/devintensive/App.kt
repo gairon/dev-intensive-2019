59b4c318f165220b8fe1c98cf2010cb9066a68f1
gairon <gairon@mail.ru>
2019-07-31 00:52:03 +0300

    HT4

--- app/src/main/java/ru/skillbranch/devintensive/App.kt
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -1,6 +1,26 @@
 package ru.skillbranch.devintensive
 
 import android.app.Application
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatDelegate
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
 
 class App : Application()  {
+    companion object {
+        private var instance: App? = null;
+
+        fun applicationContext() = instance!!.applicationContext
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        PreferencesRepository.getAppTheme().also {
+            AppCompatDelegate.setDefaultNightMode(it)
+        }
+    }
 }
